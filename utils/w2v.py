6052dc1d609924690683df981b725a71a8803a50
Daniele Sartiano <daniele.sartiano@gmail.com>
2017-02-27 22:20:10 +0100

    added argparse

--- utils/w2v.py
+++ utils/w2v.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import sys
+import gensim
+import argparse
 
 class SentenceIterator(object):
     def __init__(self, input=sys.stdin, max=1000000000):
@@ -14,7 +17,20 @@ class SentenceIterator(object):
 
 
 def main():
-    model = gensim.models.Word2Vec(SentenceIterator(), size=300, max_vocab_size=10000000,min_count=5, workers=20)                                                                                                                           
-    model.init_sims(replace=True)                                                                                                                                                                         
-    model.save('w2v.txt')   
-    model.save_word2vec_format('', fvocab=None, binary=False)
+    parser = argparse.ArgumentParser(description='word2vec')
+    parser.add_argument('-s', '--size', help='word embeddings size', type=int, default=300)
+    parser.add_argument('-m', '--max-vocab-size', help='max vocab size', type=int, default=10000000)
+    parser.add_argument('-mc', '--min-count', help='min count', type=int, default=5)
+    parser.add_argument('-w', '--workers', help='# workers', type=int, default=12)
+
+    args = parser.parse_args()
+
+    model = gensim.models.Word2Vec(SentenceIterator(), size=args.size, max_vocab_size=args.max_vocab_size, min_count=args.min_count, workers=args.workers)
+    model.init_sims(replace=True)
+    model.save('word_embeddings_gensim_%s.txt' % args.size)
+    model.save_word2vec_format('word_embeddings_%s' % args.size, , fvocab='vocab_%s' % args.size, binary=False)
+
+
+if __name__ == '__main__':
+    main()
+
