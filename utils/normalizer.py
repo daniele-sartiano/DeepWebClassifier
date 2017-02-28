918b29ec07b0815d5fd57bf0df9920a78bf089d4
Daniele Sartiano <daniele.sartiano@gmail.com>
2017-02-28 18:36:02 +0100

    convert numbers

--- utils/normalizer.py
+++ utils/normalizer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import re
 from keras.preprocessing.text import Tokenizer, text_to_word_sequence
 
 
@@ -8,7 +9,7 @@ def main():
     tokenizer = Tokenizer()
     for line in sys.stdin:
         t = line.strip()
-        print ' '.join(text_to_word_sequence(t, lower=False))
+        print re.sub(r'\d', '0', ' '.join(text_to_word_sequence(t, lower=False)))
 
 
 if __name__ == '__main__':
